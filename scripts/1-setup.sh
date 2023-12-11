03c0503b3cc1d33e7c9952f0e89734d311e79b64
anisbsalah <anisbensalah.89@gmail.com>
2023-12-11 15:27:16 +0100

    update

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -60,6 +60,7 @@ sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 
 {
+	echo 'LANG=en_US.UTF-8'
 	echo 'LC_ADDRESS=en_US.UTF-8'
 	echo 'LC_IDENTIFICATION=en_US.UTF-8'
 	echo 'LC_MEASUREMENT=en_US.UTF-8'
