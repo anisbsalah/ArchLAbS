c56c47a248f17417c0c374d9cd4f75bc0f0d4595
anisbsalah <anisbensalah.89@gmail.com>
2023-12-30 05:15:52 +0100

    update

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -26,7 +26,7 @@ echo "
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 sed -i 's/^#Color/Color\nILoveCandy/' /etc/pacman.conf
 
-pacman -S --noconfirm archlinux-keyring # update keyrings to latest to prevent packages failing to install
+pacman -Sy --noconfirm archlinux-keyring # update keyrings to latest to prevent packages failing to install
 pacman -S --noconfirm --needed gptfdisk btrfs-progs glibc
 pacman -S --noconfirm --needed pacman-contrib reflector
 pacman -S --noconfirm --needed terminus-font
@@ -36,15 +36,14 @@ setfont ter-v18b
 
 iso=$(curl -4 ifconfig.co/country-iso)
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-
 echo "
 ====================================================================
  Setting up ${iso} mirrors for faster downloads
 ====================================================================
 "
 reflector -a 48 -c "${iso}" -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
-mkdir /mnt &>/dev/null # Hiding error message if any
 
+mkdir /mnt &>/dev/null # Hiding error message if any
 echo "
 ====================================================================
  Formating Disk
@@ -142,6 +141,7 @@ echo "
 ====================================================================
 "
 pacstrap -K /mnt base base-devel linux linux-firmware btrfs-progs grub nano sudo wget bash-completion terminus-font --noconfirm --needed
+echo "keyserver hkp://keyserver.ubuntu.com" >>/mnt/etc/pacman.d/gnupg/gpg.conf
 cp -R "${SCRIPT_DIR}" /mnt/root/ArchLAbS
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
